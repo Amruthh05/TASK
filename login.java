faf87ea0af609bef821bc5d3ae95bc8d7b716c7d
amruth <amruths101@gmail.com>
2026-08-12 17:10:00 +0530

    updated login in feature branch

--- login.java
+++ login.java
@@ -1,5 +1,15 @@
 public class Login {
     public static void main(String[] args) {
-        System.out.println("Login Page");
+        String username = "admin";
+        String password = "1234";
+
+        System.out.println("Checking login details...");
+
+        if (username.equals("admin") && password.equals("1234")) {
+            System.out.println("Login successful!");
+            System.out.println("Welcome " + username);
+        } else {
+            System.out.println("Invalid username or password");
+        }
     }
 }
